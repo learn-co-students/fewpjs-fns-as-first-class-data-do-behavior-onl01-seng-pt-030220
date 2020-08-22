1e9e145819f0ddf2d430e894d14d83d479ed44a8
SincerelyBrittany <sincerelybr@gmail.com>
2020-08-22 12:01:04 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,27 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time){
+  const words = time.split(':');
+  console.log(words);
+  const parsedWords = parseInt(words, 0);
+  console.log(parsedWords)
+  if (parsedWords < 12) {
+    return "Good Morning"
+  }
+  else if (parsedWords < 17){
+    return "Good Afternoon"
+  }
+  else {
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(arg){
+  // const timeString = document.getElementById('greeting').value
+  console.log(arg)
+  let greeting = document.getElementById('greeting')
+  greeting.innerText = arg
+}
